7c8cf0a628abc7adf1225a236721874770d7342a
Eirik Haver <eiriha@gmail.com>
2011-01-19 11:13:13 +0100

    Testing a clean project

---
+++ application/testproject2/src/no/ntnu/item/testactivity.java
@@ -0,0 +1,15 @@
+package no.ntnu.item;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class testactivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
