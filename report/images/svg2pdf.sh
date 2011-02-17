be719516990020beabf1f13ea507f3903c3e83ee
Pål Ruud <ruudud@gmail.com>
2011-02-17 17:04:08 +0100

    Autocropping w00t!

--- report/images/svg2pdf.sh
+++ report/images/svg2pdf.sh
@@ -2,4 +2,4 @@
 
 filename=$(echo $1 | sed 's/\.svg//')
 
-inkscape -D -z --file=$1 --export-pdf=$filename.pdf
+inkscape -D -z --file=$1 --verb=FitCanvasToSelectionOrDrawing --export-pdf=$filename.pdf
