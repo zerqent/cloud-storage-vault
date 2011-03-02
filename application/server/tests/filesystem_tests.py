667875cbea0eb8839491659934abd0604ecb7d3c
Pål Ruud <ruudud@gmail.com>
2011-03-02 15:51:32 +0100

    Move generic code and new test for get files

--- application/server/tests/filesystem_tests.py
+++ application/server/tests/filesystem_tests.py
@@ -4,11 +4,29 @@ import random
 import string
 import os
 
-from filesystem import FileSystemException, save_file, FILE_STORE
+from filesystem import (FileSystemException, save_file, FILE_STORE,
+                        retrieve_file)
 
 class TestFileOperationPrimitives(unittest.TestCase):
+    def __create_random_file(self, contents=None):
+        si = ''.join(random.sample(string.letters+string.digits, 12))
+        fp = open(os.path.join(FILE_STORE, si), 'w')
+        if contents is not None:
+            fp.write(contents)
+        else:
+            fp.write('TESTING')
+        fp.close()
+        return si
+
     def setUp(self):
         pass
+
+    def test_get_of_file_should_return_file_pointer_yielding_correct_file(self):
+        si = self.__create_random_file('GET_TEST')
+        file_iterator, size = retrieve_file(si)
+        result = file_iterator.next()
+
+        self.assertEqual(result, 'GET_TEST')
 
     def test_save_new_file(self):
         si = ''.join(random.sample(string.letters+string.digits, 12))
@@ -22,10 +40,7 @@ class TestFileOperationPrimitives(unittest.TestCase):
         self.assertEqual(result, 'TESTING')
 
     def test_save_file_with_existing_si_but_no_we_should_fail(self):
-        si = ''.join(random.sample(string.letters+string.digits, 12))
-        fp = open(os.path.join(FILE_STORE, si), 'w')
-        fp.write('TESTING')
-        fp.close()
+        si = self.__create_random_file()
 
         self.assertRaises(FileSystemException, save_file, si, 'kjdfkj')
 
