b21cd8b0691683a61d2329ee37706411dd6707da
Pål Ruud <ruudud@gmail.com>
2011-03-02 11:44:01 +0100

    Use httplib

--- application/server/http_status_codes.py
+++ application/server/http_status_codes.py
@@ -1,7 +1,10 @@
-HTTP_CODES = {
-    '401': 'Unauthorized',
-}
+try:
+    from httplib import reponses
+except ImportError:
+    responses = {
+        '401': 'Unauthorized',
+    }
 
 def get_http_code(code):
-    if code in HTTP_CODES.keys():
-        return '%s %s' % (code, HTTP_CODES[code])
+    if code in responses.keys():
+        return '%s %s' % (code, responses[code])
