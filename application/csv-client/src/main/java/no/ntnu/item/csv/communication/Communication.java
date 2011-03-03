4969472f82a6d37d09223fa89d9fb7f78de2cfa8
Eivind Melvold <eivind.melvold@gmail.com>
2011-03-03 12:44:08 +0100

    Communication.java done

--- application/csv-client/src/main/java/no/ntnu/item/csv/communication/Communication.java
+++ application/csv-client/src/main/java/no/ntnu/item/csv/communication/Communication.java
@@ -3,18 +3,23 @@ package no.ntnu.item.csv.communication;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
 
 import no.ntnu.item.csv.csvobject.CSVObject;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.ByteArrayBody;
-import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
 
 public class Communication {
 
@@ -23,49 +28,57 @@ public class Communication {
     	if(object == null || serv_addr == null)
     		return 400;
     	
+    	
     	HttpClient client = new DefaultHttpClient();
     	HttpPost post = new HttpPost(serv_addr);
     	
-    	ByteArrayBody file = new ByteArrayBody(object.getCipherText(), object.getCapability().getStorageIndex().toString());
-//    	FileBody file = new FileBody(new File("/home/melvold/Desktop/test.txt"));
-    	MultipartEntity reqEnt = new MultipartEntity();
-    	reqEnt.addPart("encrypted_file", file);
-    	post.setEntity(reqEnt);
+    	ByteArrayBody file = new ByteArrayBody(object.getCipherText(), object.getCapability().getStorageIndex());
+    	
+    	MultipartEntity entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+    	entity.addPart("encrypted_file", file);
+    	post.setEntity(entity);
     	
     	HttpResponse response = client.execute(post);
-    	
-//    	HttpEntity respEnt = response.getEntity(); 	
-//    	if (respEnt != null) {
-//    	    InputStream is = respEnt.getContent();
-//    	    int l;
-//    	    byte[] tmp = new byte[2048];
-//    	    while ((l = is.read(tmp)) != -1);
-//    	    is.close();
-//    	    String value = new String(tmp);
-//            
-//            System.out.println(value);
-//    	}
+  	
+    	HttpEntity respEnt = response.getEntity(); 	
+    	if (respEnt != null) {
+    	    InputStream is = respEnt.getContent();
+    	    int l;
+    	    byte[] tmp = new byte[2048];
+    	    while ((l = is.read(tmp)) != -1);
+    	    is.close();
+    	    String value = new String(tmp);
+            
+            System.out.println(value);
+    	}
     	
     	return response.getStatusLine().getStatusCode();
     }
     
-    public byte[] get(String index, String serv_addr) throws ClientProtocolException, IOException
+    public static byte[] get(String index, String serv_addr) throws ClientProtocolException, IOException
     {
     	if(index == null || serv_addr == null)
     		return null;
     	
     	HttpClient client = new DefaultHttpClient();
     	HttpPost post = new HttpPost(serv_addr);
-    	
-    	StringBody str_ind = new StringBody(index);
-    	MultipartEntity reqEnt = new MultipartEntity();
-    	reqEnt.addPart("storage_index", str_ind);
+
+    	List <NameValuePair> nvp = new ArrayList <NameValuePair>();
+    	nvp.add(new BasicNameValuePair("storage_index", index));
+    	post.setEntity(new UrlEncodedFormEntity(nvp, HTTP.UTF_8));
     	
     	HttpResponse response = client.execute(post);
     	
     	InputStream is = response.getEntity().getContent();
     	ArrayList<Byte> tmp = new ArrayList<Byte>();
-    	
+//    	int l;
+//    	byte[] tmp = new byte[2048];
+//	    while ((l = is.read(tmp)) != -1);
+//	    is.close();
+//	    String value = new String(tmp);
+//        
+//        System.out.println(value);
+        
     	int nb;
     	while((nb = is.read()) != -1)
     		tmp.add((byte)nb);
@@ -78,21 +91,19 @@ public class Communication {
     	return bytes;
     }
     
-    public static void main(String[] args) throws IOException{
-//    	InputStream is = new FileInputStream(new File("/home/melvold/Desktop/test.txt"));
+//    public static void main(String[] args) throws IOException{   	
+//
+////    	CSVFileFacade file = new CSVFileFacade();
+////    	file.setPlainText(new File("/home/melvold/Desktop/test.txt"));
+////    	file.encrypt();
+////    	
+////    	put(file, "http://129.241.205.111/put");
 //    	
-//    	int nb;
-//    	while((nb = is.read()) != -1){
-//    		System.out.println(nb);
-//    	}
-    	
-//    	byte[] b = get("index", "http://129.241.205.111/test/");
-//    	if(b != null){
-//	    	for(int i = 0; i < b.length; i++){
-//	    		System.out.println((int)b[i]);
-//	    	}
-//    	}
-    	
-//    	put(null, "http://129.241.205.111/put");
-    }
+////    	byte[] b = get("", "http://129.241.205.111/get");
+////    	if(b != null){
+////	    	for(int i = 0; i < b.length; i++){
+////	    		System.out.println((int)b[i]);
+////	    	}
+////    	}
+//    }
 }
