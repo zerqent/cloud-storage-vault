4f2847344c19c79a9231b57194b6f0493a2d5c31
Pål Ruud <ruudud@gmail.com>
2011-03-24 10:19:59 +0100

    Initial commit of Initial Share Creation

---
+++ application/CSVAndroid/src/main/java/no/ntnu/item/csv/CreateShareActivity.java
@@ -0,0 +1,32 @@
+package no.ntnu.item.csv;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class CreateShareActivity extends Activity {
+	private Button bConfirmAlias;
+	private EditText eAlias;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.createshare);
+		
+		bConfirmAlias = (Button) findViewById(R.id.CreateShareConfirm);
+		eAlias = (EditText) findViewById(R.id.CreateShareAlias);
+		
+		bConfirmAlias.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), eAlias.getText(), Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+	
+	
+	
+	
+}
